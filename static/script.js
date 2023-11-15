1e3635cb7f99daca72d246309edbb631e8240acc
Valqps <116129487+Valqps@users.noreply.github.com>
2023-11-14 23:38:27 -0300

    mudança menu

--- static/script.js
+++ static/script.js
@@ -1,5 +1,19 @@
-var itensDetalhados = document.querySelectorAll(menu-detalhado);
+var itensDetalhados = document.querySelectorAll('menu-detalhado-oculto');
+var menuLateral = document.getElementById('menu-lateral');
 
-itensDetalhados.forEach(element => {
-    
-});
+// menuLateral.addEventListener('mousemove', () => {
+//     itensDetalhados.forEach(elemento => {
+//         console.log(elemento);
+//         // elemento.classList.remove('menu-detalhado-oculto')
+//     })
+// });
+
+// menuLateral.forEach(item => {   
+//     item.addEventListener("click", () => {
+//         console.log('clicou');
+//         itensDetalhados.forEach(elemento => {
+//             console.log('clicou');
+//             elemento.classList.add('menu-detalhado-exibido')
+//         })        
+//     })
+// })
